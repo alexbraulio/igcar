6189da07a1c5af6aac3408cbb82ca2ef8d08494a
nadaparafazer <worldvision3d@gmail.com>
2015-12-04 14:33:46 -0200

    Signed-off-by: nadaparafazer <worldvision3d@gmail.com>

--- conexao.php
+++ conexao.php
@@ -3,6 +3,6 @@
 //$db = mysql_connect("localhost","root","");
 $db = mysqli_connect("mysql.hostinger.com.br","u737997304_igcar","nysMdWJc3bhvPYP9Uy");
 // selecionando banco de tabelas ou banco de dados.
-$dados = mysqli_select_db("u737997304_siste", $db);
+$dados = mysql_select_db("u737997304_siste", $db);
 
 ?>
